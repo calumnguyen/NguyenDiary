972f2e33839dbe2db46b5ab8a46f162308d92a62
Priyanshu Tiwari <tpriyanshu90@gmail.com>
2021-05-22 03:07:08 +0530

    created home component boiler plate

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,14 +1,30 @@
-import React, { PureComponent } from 'react'
-import styles from './Home.module.css';
+import React, { PureComponent } from "react";
+import "./Home.css";
 
-export class Home extends PureComponent {
-    render() {
-        return (
-            <div className={styles.home}>
-                Hello guys
-            </div>
-        )
-    }
+class Profile extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-sm-12"></div>
+      </div>
+    );
+  }
 }
 
-export default Home
+export class Home extends PureComponent {
+  render() {
+    return (
+      <section className="home">
+        <div className="row">
+          <div className="col-sm-12">
+            <h3>Log In</h3>
+            <p>Choose your profile, and we will send you an email to login</p>
+            <p>Work in progress...</p>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Home;
