730f937212c4bbaacffd9bcd5f0cc126c4de20d1
Priyanshu Tiwari <tpriyanshu90@gmail.com>
2021-06-07 11:16:04 +0530

    made home page responsive for 5 profile

--- client/src/components/pages/Home/ProfileCard.js
+++ client/src/components/pages/Home/ProfileCard.js
@@ -1,10 +1,9 @@
 import React from "react";
-import profileImg from "../../../assets/img/default_profile_pics/others.png";
 
 function ProfileCard(props) {
   return (
     <div className="profileCard" onClick={() => props.handleSignInRequest({email: props.profile.information.email})}>
-      <div className="profileImg"><img src={props.profile.information.avatar}/></div>
+      <div className="profileImg"><img src={props.profile.information.avatar} className="img-fluid"/></div>
       <div className="profileName">
         <h3>{props.profile.information.fullName}</h3>
       </div>
